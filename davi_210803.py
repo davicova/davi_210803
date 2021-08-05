2953ecd3d1ddd724c09e4f35ad9f9d2d3166ce65
davicova <86981409+davicova@users.noreply.github.com>
2021-08-05 15:17:23 -0300

    Add files via upload

---
+++ davi_210803.py
@@ -0,0 +1,45 @@
+print("1 *******************************")
+paises=["brasil","brunei","mexico","argentina","siria","chile","egito"]
+print(paises)
+print("2 *******************************")
+print(paises[1])
+print("3 *******************************")
+paises.insert(8,"liechtenstein")
+print(paises)
+print("4 *******************************")
+ha=["montebranco"]
+ha.insert(0,"montenegro")
+print(ha)
+print("5 *******************************")
+paises2=["egito"," ","liechtenstein"]
+print(paises2)
+print("6 *******************************")
+name="a B"
+print(name.upper())
+print(name)
+print("7 *******************************")
+paises.insert(4,"Sumare")
+print(paises)
+print("8 *******************************")
+del paises[3]
+print(paises)
+print("9 *******************************")
+print(paises[-6])
+print("10 *******************************")
+popped_item = paises.pop()
+print(paises)
+print(popped_item)
+print("11 *******************************")
+vari="a-b"
+popped_item = vari.pop()
+print(vari)
+print(popped_item)
+
+
+
+
+
+
+
+
+
